1e1371fb6374d5b4b7dee35f4c84e6903f7ac5fa
이준석 <wnstjrl96@naver.com>
2021-05-18 23:11:06 +0900

    출력한 정보 전부 가져와서 json 형식으로 저장함

--- index.js
+++ index.js
@@ -14,10 +14,28 @@ client.login(config.token);
 client.on('message', message => {
 	console.log(message.content);
 	const messageArr = message.content.split("")
-	if(messageArr[0].match("!")){
-		const userName = message.content.replace("!", "");
-		message.channel.send(userName+"님의 정보입니다.");
-		const data = lostArk.getUserInfo(userName);
-		message.channel.send(data);
+	try{
+		if(messageArr[0].match("!")){
+			const userName = message.content.replace("!", "");
+			message.channel.send(userName+"님의 정보입니다.");
+			lostArk.getUserInfo(userName).then((data) => {
+				// description 생성
+				const userInfo = createDescription(data);
+				const embed = new Discord.MessageEmbed().setDescription(userInfo);
+				message.channel.send(embed);
+			});
+		}
+	} catch (error){
 	}
 });
+
+function createDescription(data){
+	console.log(data);
+	let body1 = "[ 기 본 정 보 ]\n";
+	body1 = body1 + `- 캐릭터명 : ${data['userName']}\n`;
+	body1 = body1 + `- 서 버 명 : ${data['server']}\n`;
+	body1 = body1 + `- 직    업 : ${data['job']}\n`;
+	body1 = body1 + `- 길    드 : ${data['guild']}\n`;
+	body1 = body1 + `- 칭    호 : ${data['title']}\n`;
+	return body1;
+}
--- lostArk.js
+++ lostArk.js
@@ -1,18 +1,95 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function getUserInfo(userName){
+function getUserInfo(userName){
     console.log(`call getUserInfo : ${userName}`);
-    let json = {};
-    await axios.get(`https://lostark.game.onstove.com/Profile/Character/`+encodeURI(userName)).then(html => {
-        const $ = cheerio.load(html.data);
-        // 닉네임
-        json['userName'] = $("span.profile-character-info__name").text();
-        // 레벨
-        json['level'] = $("span.profile-character-info__lv").text();
+    return new Promise((resolve, reject) => {
+        axios.get(`https://lostark.game.onstove.com/Profile/Character/`+encodeURI(userName)).then(html => {
+            const $ = cheerio.load(html.data);
+            let json = {};
+            // 닉네임
+            json['userName'] = $("span.profile-character-info__name").text();
+            
+            // 레벨
+            json['level'] = $("span.profile-character-info__lv").text();
+
+            // 템렙
+            $("div.level-info2__item > span").each(function(index, item){
+                if(index === 1)
+                    json['itemLevel'] = $(this).text();
+            });
+
+            // 서버명
+            json['server'] = $("span.profile-character-info__server").text().replace("@", "");
+
+            // 직업명
+            json['job'] = $("img.profile-character-info__img").attr("alt");
+
+            // 길드
+            $("div.game-info__guild > span").each(function(index, item){
+                if(index === 1)
+                    json['guild'] = $(this).text();
+            });
+
+            // 칭호
+            $("div.game-info__title > span").each(function(index, item){
+                if(index === 1)
+                    json['title'] = $(this).text();
+            });
+            // 원정대 영지 이름, f레벨
+            $("div.game-info__wisdom").children().each(function(index, item){
+                if(index === 1)
+                    json['garden_level'] = $(this).text()
+                else if (index === 2)
+                    json['garden_name'] = $(this).text();
+            });
+            // 특성 정보
+            let count = 0;
+            let temp = [];
+            $("div.profile-ability-battle > ul > li").children().each(function(index, item){
+                if($(this).attr("class") !== 'profile-ability-tooltip'){
+                     if($(this).text() !== undefined){
+                        temp[count] = $(this).text();
+                        count = count + 1;
+                     }
+                }                   
+            });
+            json['character'] = temp;
+
+            // 각인 효과
+            count = 0;
+            temp = [];
+            $("div.profile-ability-engrave > div > div > ul > li > span").each(function(index, item){                   
+                temp[count] = $(this).text();
+                count = count + 1;
+            });
+            json['ability'] = temp;
+
+            // 보유 캐릭터 직업
+            count = 0;
+            temp = [];
+            $("ul.profile-character-list__char").children().each(function(index, item){
+                $(this).children().children().children().each(function(index, item){
+                    if($(this).attr("alt") !== undefined){
+                        temp[count] = $(this).attr("alt");
+                        count = count + 1;
+                    }                  
+                });
+            });
+            json['own_job'] = temp;
+
+            // 보유 캐릭터 명
+            count = 0;
+            temp = [];
+            $("ul.profile-character-list__char > li > span > button").each(function(index, item){
+                temp[count] = $(this).attr("onclick").split("/")[3].replace("'", "");
+                count = count + 1;
+            });
+            json['own_userName'] = temp;
+
+            resolve(json);
+        });
     });
-    console.log(json);
-    return json;
 }
 
 module.exports = {getUserInfo};
