9d6a0fd772a8134f2c3a67a4ebb5065bffae097c
이준석 <wnstjrl96@naver.com>
2021-05-20 22:14:56 +0900

    전투정보실 TUI 변경 및 이벤트 정보 출력 기능 추가

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const lostArk = require("./lostArk.js");
 const config = require("./config.json");
+const lostArkImg = require("./lostArkData.json")
 // initialized
 client.once('ready', () => {
 	console.log('Ready!');
@@ -15,46 +16,108 @@ client.on('message', message => {
 	console.log(message.content);
 	const messageArr = message.content.split("")
 	try{
-		if(messageArr[0].match("!")){
+		if(messageArr[0].includes("!")){
 			const userName = message.content.replace("!", "");
-			message.channel.send(`[${userName}] 님의 정보입니다.`);
-			lostArk.getUserInfo(userName).then((data) => {
-				// description 생성
-				const userInfo = createDescription(data);
-				const embed = new Discord.MessageEmbed().setDescription(userInfo);
-				message.channel.send(embed);
-			});
+			if(userName.includes("이벤트")){
+				// 이벤트 정보 출력
+				lostArk.getEventMessageEmbed().then((data) => {
+					const embed = createEventDescription(data);
+					for(let i = 0; i < embed.length; i++)
+						message.channel.send(embed[i]);
+						setTimeout(()=>{}, 1000);
+				});
+				
+				return;
+			} else {
+				lostArk.getUserInfo(userName).then((data) => {
+					// description 생성
+					if(userName === '정점은움직이지않아'){
+						message.channel.send("*__길드장님의 고귀한 정보입니다.__* 🙇‍♂️🙇‍♀️");
+					}
+					const embed = createDescription(userName, data);			
+					message.channel.send(embed);	
+					return;	
+				});
+			}
 		}
-	} catch (error){
+	} catch (error){		
 	}
 });
 
-function createDescription(data){
-	let body1 = "[ 기 본 정 보 ]\n";
-	body1 = body1 + `- 캐릭터명 : ${data['userName']}\n`;
-	body1 = body1 + `- 서 버 명 : ${data['server']}\n`;
-	body1 = body1 + `- 직    업 : ${data['job']}\n`;
-	body1 = body1 + `- 길    드 : ${data['guild']}\n`;
-	body1 = body1 + `- 칭    호 : ${data['title']}\n\n`;
-
-	let body2 = "[ 레 벨 정 보 ]\n";
-	body2 = body2 + `- 전투Lv : ${data['level']}\n`;
-	body2 = body2 + `- 아이템Lv : ${data['itemLevel']}\n\n`;
-
-	let body3 = "[ 원 정 대 영 지 ]\n";
-	body3 = body3 + `- 영지이름 : ${data['garden_name']}\n`;
-	body3 = body3 + `- 영지Lv : ${data['garden_level']}\n\n`;
-
-	let body4 = "[ 각 인 효 과 ]\n";
+function createDescription(userName, data){
+	// 각인 효과
+	let body4 = "";
 	for(let i = 0; i < data['ability'].length; i++){
-		body4 = body4 + `- ${data['ability'][i]}\n`;
+		body4 = body4 + `${data['ability'][i]}\n`;
 	}
 	body4 = body4 + "\n";
 
-	let body5 = "[ 보 유 캐 릭 ]\n";
+	// 보유 캐릭터
+	let body5 = "";
 	for(let i = 0; i < data['own_job'].length; i++){
-		body5 = body5 + `- ${data['own_userName'][i]} / ${data['own_job'][i]}\n`;
+		body5 = body5 + `[${data['own_userName'][i]}](https://lostark.game.onstove.com/Profile/Character/${data['own_userName'][i]}) / ${data['own_job'][i]}\n`;
 	}
 
-	return body1 + body2 + body3 + body4 + body5;
+	const embed = new Discord.MessageEmbed()
+		.setColor('#0099ff')
+		.setAuthor(`__${userName}__`, lostArkImg[`${data['job']}`], `https://lostark.game.onstove.com/Profile/Character/${userName}`)
+		.addFields(
+			{
+				"name": "[ 기 본 정 보 ]", 
+				"value": `\`캐릭터명\` : ${data['userName']}\n\`서 버 명\` : ${data['server']}\n\`직  업\` : ${data['job']}\n\`길 드\` : ${data['guild']}\n\`칭 호\` : ${data['title']}`, 
+				inline: true
+			},
+			{
+				"name": "[ 각 인 효 과 ]",
+				"value": body4,
+				inline: true
+			},
+		)
+		.addFields(
+			{ name: '\u200B', value: '\u200B' },
+			{
+				"name": "[ 원 정 대 영 지 ]",
+				"value": `\`영지이름\` : ${data['garden_name']}\n\`영지Lv\` : ${data['garden_level']}\n`,
+				inline: true			
+			},
+			{
+				"name": "[ 기 본 특 성 ]",
+				"value": `\`최대생명력\` : ${data['health']}\n\`공격력\` : ${data['attack']}`,
+				inline: true
+			},
+			{
+				"name": "[ 레 벨 정 보 ]", 
+				"value": `\`전투Lv\` : ${data['level']}\n\`아이템Lv\` : ${data['itemLevel']}`,
+				inline: true
+			},
+		)
+		.addFields(
+			{ name: '\u200B', value: '\u200B' },
+			{
+				"name": "[ 보 유 캐 릭 터 ]",
+				"value": body5,
+			}
+		)
+		.setThumbnail('https://i.imgur.com/Vc11WQc_d.webp?maxwidth=760&fidelity=grand')
+		.setTimestamp()
+		.setFooter("밤에뜨는해", "https://i.imgur.com/Vc11WQc_d.webp?maxwidth=760&fidelity=grand")
+		;
+
+	return embed;
+}
+
+function createEventDescription(data){
+	let embedArr = [];
+	for(let i = 0; i < data['subject'].length; i++){
+		const embed = new Discord.MessageEmbed()
+			.setColor("#fffff")
+			.setAuthor(`${data['subject'][i]}`, "", `https://lostark.game.onstove.com${data['link'][i]}`)
+			.setImage(`${data['thumb'][i]}`)
+			.setFooter(`이벤트기간 : ${data['term'][i]}`, "")
+			;
+		console.log(`${data['thumb'][i]}`);
+		embedArr.push(embed);
+	}
+
+	return embedArr;
 }
--- lostArk.js
+++ lostArk.js
@@ -87,9 +87,57 @@ function getUserInfo(userName){
             });
             json['own_userName'] = temp;
 
+            // 공격력
+            $("div.profile-ability-basic > ul > li > span").each(function(index, item){
+                const attack = $(this).text();
+                if(attack !== "공격력" && index === 1){
+                    json['attack'] = attack;
+                }
+                const health = $(this).text();
+                if(health !== '최대생명력' && index === 3){
+                    json['health'] = health;
+                }
+            });
+
             resolve(json);
         });
     });
 }
 
-module.exports = {getUserInfo};
+function getEventMessageEmbed(){
+    console.log("call getEventMessageEmbed !!");
+    return new Promise((resolve, reject) => {
+        axios.get("https://lostark.game.onstove.com/News/Event/Now").then(html => {
+            const $ = cheerio.load(html.data);
+            let event = {};
+            // 링크
+            let temp = [];
+            $("div.list.list--event > ul > li > a").each(function(index, item){
+                temp[index] = $(this).attr("href");
+            });
+            event['link'] = temp;
+
+            // 썸네일
+            temp = [];
+            let temp2 = [];
+            $("div.list.list--event > ul > li > a > div.list__thumb > img").each(function(index, item){
+                temp[index] = $(this).attr("src");
+                temp2[index] = $(this).attr("alt");
+            });
+            event['thumb'] = temp;
+            event['subject'] = temp2;
+
+            // 이벤트 기간
+            temp = [];
+            $("div.list__term").each(function(index, item){
+                temp[index] = $(this).text().split(" : ")[1];
+            });
+            event['term'] = temp;
+
+            console.log(event);
+            resolve(event);
+        })
+    });
+}
+
+module.exports = {getUserInfo, getEventMessageEmbed};
