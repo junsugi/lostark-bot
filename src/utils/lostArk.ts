a6eba9414fa456acdc74f9e6104dc360e5b6595b
이준석 <wnstjrl96@naver.com>
2021-11-24 16:42:24 +0900

    [#Fix] async await로 구문 변경

--- src/utils/lostArk.ts
+++ src/utils/lostArk.ts
@@ -1,145 +1,132 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-function getHtml(url: string){
-    return new Promise((resolve, reject) => {
-        axios.get(`${url}`).then((html) => {
-            resolve(html);
-        });
-    });
+async function getHtml(url: string){
+    return await axios.get(`${url}`);
 }
 
 export async function getUserInfo(userName: string){
-    console.log(`call getUserInfo : ${userName}`);
-    return new Promise((resolve, reject) => {
-        getHtml(`https://lostark.game.onstove.com/Profile/Character/${encodeURI(userName)}#profile-collection`).then((html: any) => {
-            const $ = cheerio.load(html.data);
-            let json: any = {};
-            // 수집형 포인트
-            $("div.collection-status").addClass("active");
-            // 닉네임
-            json.userName = $("span.profile-character-info__name").text();
-            // 레벨
-            json.level = $("span.profile-character-info__lv").text();
-            // 템렙
-            $("div.level-info2__item > span").each(function(index, item){
-                if(index === 1)
-                    json.itemLevel = $(this).text();
-            });
-            // 서버명
-            json.server = $("span.profile-character-info__server").text().replace("@", "");
-            // 직업명
-            json.job = $("img.profile-character-info__img").attr("alt");
-            // 길드
-            $("div.game-info__guild > span").each(function(index, item){
-                if(index === 1)
-                    json.guild = $(this).text();
-            });
-            // 칭호
-            $("div.game-info__title > span").each(function(index, item){
-                if(index === 1)
-                    json.title = $(this).text();
-            });
-            // 원정대 영지 이름, f레벨
-            $("div.game-info__wisdom").children().each(function(index, item){
-                if(index === 1)
-                    json.garden_level = $(this).text()
-                else if (index === 2)
-                    json.garden_name = $(this).text();
-            });
-            
-            // 특성 정보
-            let count = 0;
-            let temp: any = [];
-            $("div.profile-ability-battle > ul > li").children().each(function(index, item){
-                if($(this).attr("class") !== 'profile-ability-tooltip'){
-                     if($(this).text() !== undefined){
-                        temp[count] = $(this).text();
-                        count = count + 1;
-                     }
-                }                   
-            });
-            json.character = temp;
+    const html: any = await getHtml(`https://lostark.game.onstove.com/Profile/Character/${encodeURI(userName)}#profile-collection`);
+    const $ = cheerio.load(html.data);
+    let json: any = {};
+    // 수집형 포인트
+    $("div.collection-status").addClass("active");
+    // 닉네임
+    json.userName = $("span.profile-character-info__name").text();
+    // 레벨
+    json.level = $("span.profile-character-info__lv").text();
+    // 템렙
+    $("div.level-info2__item > span").each(function(index, item){
+        if(index === 1)
+            json.itemLevel = $(this).text();
+    });
+    // 서버명
+    json.server = $("span.profile-character-info__server").text().replace("@", "");
+    // 직업명
+    json.job = $("img.profile-character-info__img").attr("alt");
+    // 길드
+    $("div.game-info__guild > span").each(function(index, item){
+        if(index === 1)
+            json.guild = $(this).text();
+    });
+    // 칭호
+    $("div.game-info__title > span").each(function(index, item){
+        if(index === 1)
+            json.title = $(this).text();
+    });
+    // 원정대 영지 이름, f레벨
+    $("div.game-info__wisdom").children().each(function(index, item){
+        if(index === 1)
+            json.garden_level = $(this).text()
+        else if (index === 2)
+            json.garden_name = $(this).text();
+    });
 
-            // 각인 효과
-            count = 0;
-            temp = [];
-            $("div.profile-ability-engrave > div > div > ul > li > span").each(function(index, item){                   
+    // 특성 정보
+    let count = 0;
+    let temp: any = [];
+    $("div.profile-ability-battle > ul > li").children().each(function(index, item){
+        if($(this).attr("class") !== 'profile-ability-tooltip'){
+            if($(this).text() !== undefined){
                 temp[count] = $(this).text();
                 count = count + 1;
-            });
-            json.ability = temp;
+            }
+        }
+    });
+    json.character = temp;
 
-            // 보유 캐릭터 직업
-            count = 0;
-            temp = [];
-            $("ul.profile-character-list__char").children().each(function(index, item){
-                $(this).children().children().children().each(function(index, item){
-                    if($(this).attr("alt") !== undefined){
-                        temp[count] = $(this).attr("alt");
-                        count = count + 1;
-                    }                  
-                });
-            });
-            json.own_job = temp;
+    // 각인 효과
+    count = 0;
+    temp = [];
+    $("div.profile-ability-engrave > div > div > ul > li > span").each(function(index, item){
+        temp[count] = $(this).text();
+        count = count + 1;
+    });
+    json.ability = temp;
 
-            // 보유 캐릭터 명
-            count = 0;
-            temp = [];
-            $("ul.profile-character-list__char > li > span > button").each(function(index, item){
-                temp[count] = $(this).attr("onclick")?.split("/")[3].replace("'", "");
+    // 보유 캐릭터 직업
+    count = 0;
+    temp = [];
+    $("ul.profile-character-list__char").children().each(function(index, item){
+        $(this).children().children().children().each(function(index, item){
+            if($(this).attr("alt") !== undefined){
+                temp[count] = $(this).attr("alt");
                 count = count + 1;
-            });
-            json.own_userName = temp;
-
-            // 공격력
-            $("div.profile-ability-basic > ul > li > span").each(function(index, item){
-                const attack = $(this).text();
-                if(attack !== "공격력" && index === 1){
-                    json.attack = attack;
-                }
-                const health = $(this).text();
-                if(health !== '최대생명력' && index === 3){
-                    json.health = health;
-                }
-            });
-            resolve(json);
+            }
         });
     });
+    json.own_job = temp;
+
+    // 보유 캐릭터 명
+    count = 0;
+    temp = [];
+    $("ul.profile-character-list__char > li > span > button").each(function(index, item){
+        temp[count] = $(this).attr("onclick")?.split("/")[3].replace("'", "");
+        count = count + 1;
+    });
+    json.own_userName = temp;
+
+    // 공격력
+    $("div.profile-ability-basic > ul > li > span").each(function(index, item){
+        const attack = $(this).text();
+        if(attack !== "공격력" && index === 1){
+            json.attack = attack;
+        }
+        const health = $(this).text();
+        if(health !== '최대생명력' && index === 3){
+            json.health = health;
+        }
+    });
+    return json;
 }
 
-export function getEventMessageEmbed(){
-    console.log("call getEventMessageEmbed !!");
-    return new Promise((resolve, reject) => {
-        getHtml("https://lostark.game.onstove.com/News/Event/Now").then((html: any) => {
-            const $ = cheerio.load(html.data);
-            let event: any = {};
-            // 링크
-            let temp: any = [];
-            $("div.list.list--event > ul > li > a").each(function(index, item){
-                temp[index] = $(this).attr("href");
-            });
-            event.link = temp;
-
-            // 썸네일
-            temp = [];
-            let temp2: any = [];
-            $("div.list.list--event > ul > li > a > div.list__thumb > img").each(function(index, item){
-                temp[index] = $(this).attr("src");
-                temp2[index] = $(this).attr("alt");
-            });
-            event['thumb'] = temp;
-            event['subject'] = temp2;
-
-            // 이벤트 기간
-            temp = [];
-            $("div.list__term").each(function(index, item){
-                temp[index] = $(this).text().split(" : ")[1];
-            });
-
-            event['term'] = temp;
-
-            resolve(event);
-        })
+export async function getEventMessageEmbed(){
+    const html: any = await getHtml("https://lostark.game.onstove.com/News/Event/Now");
+    const $ = cheerio.load(html.data);
+    let event: any = {};
+    // 링크
+    let temp: any = [];
+    $("div.list.list--event > ul > li > a").each(function(index, item){
+        temp[index] = $(this).attr("href");
     });
+    event.link = temp;
+
+    // 썸네일
+    temp = [];
+    let temp2: any = [];
+    $("div.list.list--event > ul > li > a > div.list__thumb > img").each(function(index){
+        temp[index] = $(this).attr("src");
+        temp2[index] = $(this).attr("alt");
+    });
+    event.thumb = temp;
+    event.subject = temp2;
+
+    // 이벤트 기간
+    temp = [];
+    $("div.list__term").each(function(index){
+        temp[index] = $(this).text().split(" : ")[1];
+    });
+    event.term = temp;
+
+    return event;
 }
